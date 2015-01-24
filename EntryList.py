64b701e5a41e3fe3b09c914121001deb5479c178
Justin McBride <justin@dare599z.com>
2015-01-24 13:36:26 -0700

    added preliminary db and queue access

---
+++ EntryList.py
@@ -0,0 +1,34 @@
+import sqlite3
+import collections
+
+class EntryList:
+
+	def __init__(self, obj=None):
+		self.conn = sqlite3.connect('data.db')
+		self.c = conn.cursor()
+		self.queue = collections.deque()
+
+	def add(self, obj):
+		self.queue.append(obj)
+		#add to db
+
+	def modify(self, obj):
+		#find, modify, return
+
+	def remove(self, eid, duration):
+		# iterate queue, find by id, remove
+
+	def getById(self, x):
+		for item in self.queue:
+			if item.eid == x:
+				return item
+		return None
+
+	def getByPos(self, x):
+		if x > len(o) or x < 0:
+			return None
+		return self.queue[x]
+
+	def getAll(self):
+		return self.queue
+
