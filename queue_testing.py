658018fb0cc551f43af07c4c01ed52851af61eac
alne4294 <alexiarose101@gmail.com>
2015-01-30 19:41:52 -0700

    adding changes even though they don't really work

--- queue_testing.py
+++ queue_testing.py
@@ -5,6 +5,7 @@ import main
 import unittest
 import tempfile
 import json
+from time import sleep
 from httplib import HTTPConnection
 
 class FlaskTestCase(unittest.TestCase):
@@ -14,6 +15,10 @@ class FlaskTestCase(unittest.TestCase):
         main.app.config['TESTING'] = True
         self.app = main.app.test_client()
 
+        entries = self.app.get('/api/1.0/queue')
+        print "first:" 
+        print entries.data
+
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(main.app.config['DATABASE'])
@@ -21,27 +26,71 @@ class FlaskTestCase(unittest.TestCase):
     #=========================================================
     #TEST GET
 
-    def test_getByPos(self):
-        entry = self.app.get('/api/1.0/queue/pos/1')
-        jsonEntry = json.loads(entry.data)
-        self.assertEqual(jsonEntry['data'], "No entry at position: 1")
-        self.assertEqual(entry.status_code, 200)
+    # def test_getByPos(self):
+    #     entry = self.app.get('/api/1.0/queue/pos/1')
+    #     jsonEntry = json.loads(entry.data)
+    #     self.assertEqual(jsonEntry['data'], "No entry at position: 1")
+    #     self.assertEqual(entry.status_code, 200)
 
-    def test_getById(self):
-        entry = self.app.get('/api/1.0/queue/id/"12345"')
-        jsonEntry = json.loads(entry.data)
-        self.assertEqual(jsonEntry['data'], 'No entry at ID: "12345"')
-        self.assertEqual(entry.status_code, 200)
+    # def test_getById(self):
+    #     entry = self.app.get('/api/1.0/queue/id/"12345"')
+    #     jsonEntry = json.loads(entry.data)
+    #     self.assertEqual(jsonEntry['data'], 'No entry at ID: "12345"')
+    #     self.assertEqual(entry.status_code, 200)
 
-    def test_getPostQueue(self):
-        entry = self.app.get('/api/1.0/queue')
-        self.assertEqual(entry.status_code, 200)
+    # def test__get_getPostQueue(self):
+    #     entry = self.app.get('/api/1.0/queue')
+    #     self.assertEqual(entry.status_code, 200)
 
     #=========================================================
     #TEST DELETE
 
     #=========================================================
-    #TEST POST
+    #TEST POST and DELETE
+
+    def test_post_getPostQueue(self):
+
+
+
+        data = '{"name":"Harry", "course":"Potions", "location":"Dungeons"}'
+        method = 'POST'
+        uri = '/api/1.0/queue'
+        baseURI = "127.0.0.1:5000"
+
+        conn = HTTPConnection(baseURI)
+        conn.request(method, uri, data)
+        res = conn.getresponse()
+
+        #return (res.status, res.reason, res.read())
+        #print str(res.status) + str(res.reason) + str(res.read())
+
+        #(status, reason, obj) = self.handle_request('POST', '/api/1.0/queue', data)
+        # return (status, reason, self.createEntryFromResponse(obj))
+        # print str(res.status) + str(res.reason) + str(res.read())#self.createEntryFromResponse(obj))
+
+        sleep(5)
+
+        entries = self.app.get('/api/1.0/queue')
+        print "second:"
+        print entries.data
+
+        # entry = self.app.get('/api/1.0/queue/pos/0')
+        # jsonEntry = json.loads(entry.data)
+        # self.assertEqual(jsonEntry['data']['name'], "Harry")
+        # self.assertEqual(entry.status_code, 200)
+
+
+
+    # def handle_request(self, method, uri, data = None):
+    #     conn = HTTPConnection(self.baseURI())
+    #     conn.request(method, uri, data)
+    #     res = conn.getresponse()
+    #     return (res.status, res.reason, res.read())
+
+    # def create(self, name, course, location):
+    #     data = '{"name":"' + name + '", "course":"' + course + '", "location":"' + location + '"}'
+    #     (status, reason, obj) = self.handle_request('POST', '/api/1.0/queue', data)
+    #     return (status, reason, self.createEntryFromResponse(obj))
 
     #=========================================================
     #TEST PUT
