964dbb7c51b9864bdd7d90a406a5cb9924925cc2
David Baird <david.baird@colorado.edu>
2015-01-30 20:13:10 -0700

    Made url parameters to strings before concatenation

--- client.py
+++ client.py
@@ -10,13 +10,16 @@ class HelpRequests:
         if response_obj["error"]:
             return None
         b = response_obj["data"]
-        return b
+        print "#################"
+        print json.loads(b)
+        return entry(jsonStr=b)
 
     def createListOfEntryFromResponse(self, response):
+#        response = response.replace("\\", "")
         response_obj =json.loads(response)
         if response_obj["error"]:
             return None
-        return [entry(elt) for elt in response_obj["data"]]
+        return [entry(jsonStr=json.loads(elt)) for elt in response_obj["data"]]
 
     def baseURI(self):
         return "127.0.0.1:5000"
@@ -32,27 +35,27 @@ class HelpRequests:
         return (status, reason, self.createListOfEntryFromResponse(obj))
 
     def getByPosition(self, pos):
-        (status, reason, obj) = self.handle_request('GET', '/api/1.0/queue/pos/' + pos, None)
+        (status, reason, obj) = self.handle_request('GET', '/api/1.0/queue/pos/' + str(pos), None)
         return (status, reason, self.createEntryFromResponse(obj))
 
     def getById(self, entryId):
-        (status, reason, obj) = self.handle_request('GET', '/api/1.0/queue/id/' + entryId, None)
-        return (status, reason, createEntryFromResponse(obj))
+        (status, reason, obj) = self.handle_request('GET', '/api/1.0/queue/id/' + str(entryId), None)
+        return (status, reason, self.createEntryFromResponse(obj))
     
     def create(self, name, course, location):
-        data = '{"name":"' + name + '", "course":"' + course + '", "location":"' + location + '"}'
+        data = '{"name":"' + name + '", "course":"' + course + '", "location":"' + str(location) + '"}'
         (status, reason, obj) = self.handle_request('POST', '/api/1.0/queue', data)
         return (status, reason, self.createEntryFromResponse(obj))
 
     def delete(self, entryId):
-        (status, reason, obj) = self.handle_request('DELETE', '/api/1.0/queue/id/' + entryId, None)
+        (status, reason, obj) = self.handle_request('DELETE', '/api/1.0/queue/id/' + str(entryId), None)
         return (status, reason, self.createEntryFromResponse(obj))
 
     def modify(self, entryId, entry):
-        (status, reason, obj) = self.handle_request('PUT', '/api/1.0/modify/id/' + entryId, entry)
+        (status, reason, obj) = self.handle_request('PUT', '/api/1.0/modify/id/' + str(entryId), entry.format())
         return (status, reason, self.createEntryFromResponse(obj))
 
     def dequeue(self, entryId, entry):
-        (status, reason, obj) = self.handle_request('PUT', '/api/1.0/dequeue/id/' + entryId, entry)
+        (status, reason, obj) = self.handle_request('PUT', '/api/1.0/dequeue/id/' + str(entryId), entry.format())
         return (status, reason, self.createEntryFromResponse(obj))
         
